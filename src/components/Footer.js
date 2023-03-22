85bb7f10f5145e53689990d32842c497cb8e9e25
Rafaeli Rangel <rafaelirangel@icloud.com>
2023-03-22 08:25:42 -0400

    Basic footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,22 @@
 import React from "react";
 import './Footer.css';
+import LanguageIcon from '@mui/icons-material/Language';
 
 function Footer() {
     return (
         <div className="footer">
-            <h1>Footer Component</h1>
+
+            <div className="footerLeft">
+                <p>© 2023 Bark Airbnb</p>
+                <p> · Terms · Sitemap · Privacy · Your Privacy Choices · Destinations</p>
+            </div>
+            
+
+            <div className="footerRight">
+                <LanguageIcon /> 
+                <p> English (US) </p>
+                <p> $ USD </p>
+            </div>
         </div>
     )
 }
