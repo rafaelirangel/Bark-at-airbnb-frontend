44a1b59e755ed4c12c17277952a8c9fd06f7dc35
Rafaeli Rangel <rafaelirangel@icloud.com>
2023-03-22 09:56:44 -0400

    Create cards with dummy data

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,164 @@
 import React from "react";
 import './Home.css';
+import Card from "../components/Card";
 
 function Home() {
     return (
         <div className="home">
-            <h1>Home Component</h1>
 
+            <div className="cards">
+                <Card
+                    src="https://a0.muscache.com/im/pictures/15159c9c-9cf1-400e-b809-4e13f286fa38.jpg?im_w=720"
+                    title="Madison, New Hampshire"
+                    description="Mountain and lake views"
+                    date="Apr 9 – 14"
+                    price="$ 400"
+                />
+                <Card
+                    src="https://a0.muscache.com/im/pictures/15159c9c-9cf1-400e-b809-4e13f286fa38.jpg?im_w=720"
+                    title="Madison, New Hampshire"
+                    description="Mountain and lake views"
+                    date="Apr 9 – 14"
+                    price="$ 400"
+                />
+                <Card
+                    src="https://a0.muscache.com/im/pictures/15159c9c-9cf1-400e-b809-4e13f286fa38.jpg?im_w=720"
+                    title="Madison, New Hampshire"
+                    description="Mountain and lake views"
+                    date="Apr 9 – 14"
+                    price="$ 400"
+                />
+                <Card
+                    src="https://a0.muscache.com/im/pictures/15159c9c-9cf1-400e-b809-4e13f286fa38.jpg?im_w=720"
+                    title="Madison, New Hampshire"
+                    description="Mountain and lake views"
+                    date="Apr 9 – 14"
+                    price="$ 400"
+                />
+            </div>
 
+            <div className="cards">
+                <Card
+                    src="https://a0.muscache.com/im/pictures/15159c9c-9cf1-400e-b809-4e13f286fa38.jpg?im_w=720"
+                    title="Madison, New Hampshire"
+                    description="Mountain and lake views"
+                    date="Apr 9 – 14"
+                    price="$ 400"
+                />
+                <Card
+                    src="https://a0.muscache.com/im/pictures/15159c9c-9cf1-400e-b809-4e13f286fa38.jpg?im_w=720"
+                    title="Madison, New Hampshire"
+                    description="Mountain and lake views"
+                    date="Apr 9 – 14"
+                    price="$ 400"
+                />
+                <Card
+                    src="https://a0.muscache.com/im/pictures/15159c9c-9cf1-400e-b809-4e13f286fa38.jpg?im_w=720"
+                    title="Madison, New Hampshire"
+                    description="Mountain and lake views"
+                    date="Apr 9 – 14"
+                    price="$ 400"
+                />
+                <Card
+                    src="https://a0.muscache.com/im/pictures/15159c9c-9cf1-400e-b809-4e13f286fa38.jpg?im_w=720"
+                    title="Madison, New Hampshire"
+                    description="Mountain and lake views"
+                    date="Apr 9 – 14"
+                    price="$ 400"
+                />
+            </div>
+
+            <div className="cards">
+                <Card
+                    src="https://a0.muscache.com/im/pictures/15159c9c-9cf1-400e-b809-4e13f286fa38.jpg?im_w=720"
+                    title="Madison, New Hampshire"
+                    description="Mountain and lake views"
+                    date="Apr 9 – 14"
+                    price="$ 400"
+                />
+                <Card
+                    src="https://a0.muscache.com/im/pictures/15159c9c-9cf1-400e-b809-4e13f286fa38.jpg?im_w=720"
+                    title="Madison, New Hampshire"
+                    description="Mountain and lake views"
+                    date="Apr 9 – 14"
+                    price="$ 400"
+                />
+                <Card
+                    src="https://a0.muscache.com/im/pictures/15159c9c-9cf1-400e-b809-4e13f286fa38.jpg?im_w=720"
+                    title="Madison, New Hampshire"
+                    description="Mountain and lake views"
+                    date="Apr 9 – 14"
+                    price="$ 400"
+                />
+                <Card
+                    src="https://a0.muscache.com/im/pictures/15159c9c-9cf1-400e-b809-4e13f286fa38.jpg?im_w=720"
+                    title="Madison, New Hampshire"
+                    description="Mountain and lake views"
+                    date="Apr 9 – 14"
+                    price="$ 400"
+                />
+            </div>
+
+            <div className="cards">
+                <Card
+                    src="https://a0.muscache.com/im/pictures/15159c9c-9cf1-400e-b809-4e13f286fa38.jpg?im_w=720"
+                    title="Madison, New Hampshire"
+                    description="Mountain and lake views"
+                    date="Apr 9 – 14"
+                    price="$ 400"
+                />
+                <Card
+                    src="https://a0.muscache.com/im/pictures/15159c9c-9cf1-400e-b809-4e13f286fa38.jpg?im_w=720"
+                    title="Madison, New Hampshire"
+                    description="Mountain and lake views"
+                    date="Apr 9 – 14"
+                    price="$ 400"
+                />
+                <Card
+                    src="https://a0.muscache.com/im/pictures/15159c9c-9cf1-400e-b809-4e13f286fa38.jpg?im_w=720"
+                    title="Madison, New Hampshire"
+                    description="Mountain and lake views"
+                    date="Apr 9 – 14"
+                    price="$ 400"
+                />
+                <Card
+                    src="https://a0.muscache.com/im/pictures/15159c9c-9cf1-400e-b809-4e13f286fa38.jpg?im_w=720"
+                    title="Madison, New Hampshire"
+                    description="Mountain and lake views"
+                    date="Apr 9 – 14"
+                    price="$ 400"
+                />
+            </div>
+            <div className="cards">
+                <Card
+                    src="https://a0.muscache.com/im/pictures/15159c9c-9cf1-400e-b809-4e13f286fa38.jpg?im_w=720"
+                    title="Madison, New Hampshire"
+                    description="Mountain and lake views"
+                    date="Apr 9 – 14"
+                    price="$ 400"
+                />
+                <Card
+                    src="https://a0.muscache.com/im/pictures/15159c9c-9cf1-400e-b809-4e13f286fa38.jpg?im_w=720"
+                    title="Madison, New Hampshire"
+                    description="Mountain and lake views"
+                    date="Apr 9 – 14"
+                    price="$ 400"
+                />
+                <Card
+                    src="https://a0.muscache.com/im/pictures/15159c9c-9cf1-400e-b809-4e13f286fa38.jpg?im_w=720"
+                    title="Madison, New Hampshire"
+                    description="Mountain and lake views"
+                    date="Apr 9 – 14"
+                    price="$ 400"
+                />
+                <Card
+                    src="https://a0.muscache.com/im/pictures/15159c9c-9cf1-400e-b809-4e13f286fa38.jpg?im_w=720"
+                    title="Madison, New Hampshire"
+                    description="Mountain and lake views"
+                    date="Apr 9 – 14"
+                    price="$ 400"
+                />
+            </div>
         </div>
     )
 }
