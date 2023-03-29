09fce9851585fea15c796e35448dd443093b4f1c
Taylor Weirtz <teweirtz@gmail.com>
2023-03-29 18:26:53 -0400

    login function

--- src/App.js
+++ src/App.js
@@ -2,10 +2,32 @@ import './App.css';
 import Home from './pages/Home';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import LogIn from './components/LogIn/LogIn';
+import LogOut from './components/LogOut/LogOut';
 import { Routes, Route } from 'react-router-dom';
 import { useState, useEffect } from 'react';
+import axios from 'axios'
 
 function App() {
+
+  const [user] = useState({
+    email: '',
+    password: '',
+    isLoggedIn: false  
+  })
+  async function handleLogIn(e) {
+    e.preventDefault()
+    axios.post('http://localhost:3001/users/login', {
+      email: user.email,
+      password: user.password
+    })
+    .then(response => {
+      localStorage.token = response.data.token
+      this.setState({ isLoggedIn: true })
+    })
+    .catch(err => console.log(err))
+  }
+
   return (
     <div className="app">
           <Header />
